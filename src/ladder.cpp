4366c7b4b3d3b628b99aa0fb6219f6ca038478f8
MehekB <mehekbhatnagar3@gmail.com>
2025-03-16 05:16:22 +0000

    fixed bug in print_word_ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -82,7 +82,7 @@ void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
         cout << "No word ladder found." << endl;
     } else {
-        output << "Word ladder found: ";
+        cout << "Word ladder found: ";
         for (const string& word : ladder) {
             cout << word << " ";
         }
