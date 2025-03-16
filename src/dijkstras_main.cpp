56bcc54dec72bb9f11d843061e29740630774a98
MehekB <mehekbhatnagar3@gmail.com>
2025-03-16 04:25:21 +0000

    updated dijkstra's to output correctly

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -4,20 +4,14 @@ int main() {
     Graph G;
     file_to_graph("small.txt", G);
 
+    int src = 0;
+    vector<int> previous(G.numVertices, -1);
+
+    vector<int> distances = dijkstra_shortest_path(G, src, previous);
+
     for (int source = 0; source < G.numVertices; ++source) {
-        cout << "Source vertex: " << source << endl;
-
-        vector<int> previous(G.numVertices, -1);
-        vector<int> distances = dijkstra_shortest_path(G, source, previous);
-
-        // Loop through all vertices to print the shortest path from source
-        for (int destination = 0; destination < G.numVertices; ++destination) {
-            if (source != destination) {
-                vector<int> path = extract_shortest_path(distances, previous, destination);
-                int totalWeight = distances[destination];
-                print_path(path, totalWeight);
-            }
-        }
+         vector<int> path = extract_shortest_path(distances, previous, source);
+         print_path(path, distances[source]);
     }
 
     return 0;
